89b4c04b3a078567a3d74d2d6a39db123280cd97
nikolay5r <nradilov30@gmail.com>
2021-10-22 14:33:28 +0300

    initialize create cube page

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const homeController = require("./controllers/homeController");
+const cubeController = require("./controllers/cubeController")
 
 const router = express.Router();
 
 router.use(homeController);
+router.use(cubeController);
 
 module.exports = router;
