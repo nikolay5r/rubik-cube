0dde973b6d2676290a00e3350983296ba5233ef9
nikolay5r <nradilov30@gmail.com>
2021-10-26 08:52:45 +0300

    Extract render attach page action in cubeAccessory controller

---
+++ src/controllers/cubeAccessoryController.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cubeService = require('../services/cubeService');
+const accessoryService = require('../services/accessoryService');
+
+const router = express.Router();
+
+async function renderAttachPage(req, res) {
+    let cube = await cubeService.getById(req.params.id)
+    let accessories = await accessoryService.getAll()
+
+    res.render('attachAccessory', { cube, accessories })
+}
+
+function attachAccessory(req, res) {
+    console.log(req)
+    res.redirect('/')
+}
+
+router.get('/attach/:id', renderAttachPage)
+router.post('/attach/:id', attachAccessory)
+
+module.exports = router;
