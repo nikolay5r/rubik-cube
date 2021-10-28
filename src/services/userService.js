7a968e343c0ec47112b89a312eface106dcf1ff6
nikolay5r <nradilov30@gmail.com>
2021-10-28 17:43:07 +0300

    Get user by username

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,8 +2,11 @@ const User = require('../models/User');
 
 const create = (username, password) => User.create({ username, password });
 
+const getByUsername = (name) => User.findOne({ username: name }).lean();
+
 const userService = {
     create,
+    getByUsername,
 
 }
 
