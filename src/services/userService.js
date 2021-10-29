54ab027924c61b9bb60fce15864a78b630d1522a
nikolay5r <nradilov30@gmail.com>
2021-10-29 23:40:02 +0300

    Get user by id

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,9 +4,12 @@ const create = (username, password) => User.create({ username, password });
 
 const getByUsername = (name) => User.findOne({ username: name }).lean();
 
+const getById = (id) => User.findById(id).lean();
+
 const userService = {
     create,
     getByUsername,
+    getById
 }
 
 module.exports = userService;
